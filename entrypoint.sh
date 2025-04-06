78b02b8ab0f9dba575647a23430e66db3c051f9b
AKILA BANDARA <akilaprabash96@gmail.com>
2025-04-06 09:48:53 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# Start the SSH server in the background
-/usr/sbin/sshd
+# Ensure SSH host keys are generated and the SSH service is started
+/usr/sbin/sshd -D &
 
 # Run the Flask application as appuser
 exec su appuser -c "python /app/app7.py"
