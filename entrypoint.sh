97fc57fbcf6a161ecd16de7da5ccd5aec6604699
AKILA BANDARA <akilaprabash96@gmail.com>
2025-04-06 10:01:32 +0530

    Delete entrypoint.sh

--- entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Ensure SSH host keys are generated and the SSH service is started
-/usr/sbin/sshd -D &
-
-# Run the Flask application as appuser
-exec su appuser -c "python /app/app7.py"
